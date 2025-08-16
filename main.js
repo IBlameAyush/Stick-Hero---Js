20d7d1121b9328fdb1f7d81ecb5a48aa9caa1990
Ayush Yadav <ayushkumar5806@gmail.com>
2025-08-16 14:16:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, 'StickHero.ico'),
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
